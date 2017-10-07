5aceb8ad377527698b87114a658411af9fb57111
Petar Vlahu <vlahupetar@gmail.com>
2017-10-07 17:30:53 -0500

    added basic logic for extracting the token

--- examples/simple-card-capture/app.js
+++ examples/simple-card-capture/app.js
@@ -2,7 +2,9 @@
 
 const fs = require('fs');
 const path = require('path');
+const http = require('https')
 const express = require('express');
+const inquirer = require('inquirer');
 
 let opts = {}
 if (fs.existsSync(__dirname+'/config.json')){
@@ -13,7 +15,6 @@ const argOptions = require('nomnom')
 	.options({
 		'tract-payments-url': {
 			position: 0,
-			required: !opts['tract-payments-url'],
 			help: 'the tract payments url',
 		},
 		port: {
@@ -38,19 +39,89 @@ if (opts.save){
 	}, undefined, 2))
 }
 
+const questions = [{
+	type: 'input',
+	name: 'username',
+	message: 'tract username'
+}, {
+	type: 'password',
+	name: 'password',
+	mask: '*',
+	message: 'tract password'
+}]
+
+if (!opts['tract-payments-url']){
+	questions.unshift({
+		type: 'input',
+		name: 'tract-payments-url',
+		message: 'please provide the host to tract'
+	})
+	
+}
+
+
 const app = express();
+inquirer.prompt(questions).then(anwsers => {
+	const alloptions = {
+		...opts,
+		...anwsers
+	}
 
-const index = fs
-    .readFileSync(path.join(__dirname, '.', '/index.html'), 'utf-8')
-    .replace(/__TRACT_PAYMENTS_URL__/g, opts['tract-payments-url']);
+	const reqOptions = {
+		hostname: `${alloptions['tract-payments-url']}`,
+		path: '/t/s/r/1.28/payments/referrerToken',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/xml',
+			Authorization:
+				'Basic ' + (new Buffer(alloptions.username + ':' + alloptions.password).toString('base64'))
+		},
+	}
+	const tokenExtractor = /referrerToken="(.*?)"/
+	console.log('submiting a request: ', JSON.stringify(reqOptions, undefined, 2))
+	http.request(reqOptions, res => {
+		const data = []
+		console.log('in res')
+		res.on('data', chunk => {
+			data.push(chunk);
+		});
+		res.on('end', () => {
+			const result = data.join('');
 
-app.get('/', function (req, res) { res.send(index) });
-app.get('/index.html', function (req, res) { res.send(index) });
+			const token = tokenExtractor.exec(result);
 
-app.use(express.static(__dirname));
-app.use('/', express.static(__dirname + '/../../public'));
+			if(token[1]){
+				console.log(`referrer token is: ${token[1]}`)
+			} else {
+				console.error('we coulndt extract the token')
+			}
+
+			const index = fs
+			.readFileSync(path.join(__dirname, '.', '/index.html'), 'utf-8')
+			.replace(/__TRACT_PAYMENTS_URL__/g, alloptions['tract-payments-url']);
+		
+			app.get('/', function (req, res) { res.send(index) });
+			app.get('/index.html', function (req, res) { res.send(index) });
+		
+			app.use(express.static(__dirname));
+			app.use('/', express.static(__dirname + '/../../public'));
+		
+		
+			app.listen(alloptions.port, function() {
+				console.log('Started client at http://localhost:' + alloptions.port);
+			});
+		
+		})
+	}).write(`
+	<?xml version="1.0" encoding="UTF-8"?>
+	<generatePaymentCollectionReferrerToken xmlns="http://www.tractbilling.com/billing/1_28/domain">
+			<errorUrl>http://www.yahoo.com?error</errorUrl>
+			<cancelUrl>http://www.yahoo.com?cancel/</cancelUrl>
+			<completeUrl>http://www.yahoo.com?complete/</completeUrl>
+	</generatePaymentCollectionReferrerToken>
+	`);
+
+})
 
 
-app.listen(opts.port, function() {
-	console.log('Started client at http://localhost:' + opts.port);
-});
+
